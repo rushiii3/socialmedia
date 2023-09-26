4a40bb8a9ba6b57523309249f74a2326ba35bf68
hrushii <hrushiop@gmail.com>
2023-09-26 21:12:06 +0530

    added token

--- BackEnd/Controllers/UserController.js
+++ BackEnd/Controllers/UserController.js
@@ -39,7 +39,8 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 const createActivationToken = (user) => {
-  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+  
+  return jwt.sign(user, `${process.env.ACTIVATION_SECRET}`, {
     expiresIn: "5m",
   });
 };
